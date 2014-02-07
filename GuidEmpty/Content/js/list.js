38a31ab4dbccd36f3d41eabb47128d400d93bc29
sharkmandan <sharkmandan.github@outlook.com>
2014-02-07 13:25:15 -0800

    initial commit

---
+++ GuidEmpty/Content/js/list.js
@@ -0,0 +1,5 @@
+﻿$(document).ready(function () {
+	$('#list-go').on('click', function () {
+		$('#list-output').val("'" + $('#list-source').val().replace(/'/g, "''").split('\n').join("',\n'") + "'").focus();
+	});
+});
---
+++ GuidEmpty/Content/js/lunch.js
@@ -0,0 +1,19 @@
+﻿$(function () {
+	choose();
+	$('#lunch-go').on('click', choose);
+
+	function choose() {
+		var choices = document.getElementById('lunch-source').value.split('\n');
+		Math.random(); Math.random(); Math.random();
+		var choice = choices[Math.floor(Math.random() * choices.length)];
+		if (choice !== '') {
+			if (choice === "Siggy's") { choice = "Siggy's (are you sure you want to ruin your day?)";}
+			var output = document.getElementById('lunch-output');
+			output.innerHTML = choice;
+			output.className = 'done';
+		}
+		else {
+			choose();
+		}
+	}
+});
