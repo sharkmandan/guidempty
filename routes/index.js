fffa4acd422fe3a3fe32e9d340bccf3555e5930a
Daniel Cairney <sharkmandan@smd-rmbp.home>
2014-09-02 22:15:58 -0700

    Initial node commit

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { title: '00000000-0000-0000-0000-000000000000', guidempty: '00000000-0000-0000-0000-000000000000' });
+});
+
+module.exports = router;
